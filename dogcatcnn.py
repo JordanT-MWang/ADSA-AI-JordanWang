7067705035a148386f72f2d82d7340f0a913f726
jordan wang <jordanw7@hawaii.edu>
2025-10-08 11:48:27 -1000

    dog test

---
+++ dogcatcnn.py
@@ -0,0 +1,55 @@
+#now cnn  
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("num pgu aval: ", len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# Define paths
+base_path = '/home/jordanw7/koa_scratch/dogcattest/catdog-data'
+train_path = os.path.join(base_path, 'train')
+valid_path = os.path.join(base_path, 'valid')
+test_path = os.path.join(base_path, 'test')
+#putting the data into a way kares expects
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path, target_size=(224,224), classes=['cat','dog'], batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=valid_path, target_size=(224,224), classes=['cat','dog'], batch_size=10)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path, target_size=(224,224), classes=['cat','dog'], batch_size=10, shuffle = False)
+assert train_batches.n == 1000
+assert valid_batches.n == 200
+assert test_batches.n == 100
+assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+imgs, labels = next(train_batches)
+
+#model 
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same', input_shape=(224,224,3)),
+    MaxPool2D(pool_size=(2,2), strides=2),
+    Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'),
+    MaxPool2D(pool_size=(2,2), strides=2),
+    Flatten(),
+    Dense(units=2, activation='softmax')
+])
+model.summary()
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
+#predictions
+test_imgs, test_labels = next(test_batches)
+predictions = model.predict(x=test_batches, verbose=0)
+for i in predictions:
+    if (np.round(predictions[i]) ==  test_labels[i]):
+        print( "correct: " + str(i))
