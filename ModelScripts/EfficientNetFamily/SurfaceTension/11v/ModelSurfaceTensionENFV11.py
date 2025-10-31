c0cf9d4e96543c31b447b7fcd319f7c6182d29be
jordan wang <jordanw7@hawaii.edu>
2025-10-30 14:43:23 -1000

    changes

--- ModelScripts/EfficientNetFamily/SurfaceTension/11v/ModelSurfaceTensionENFV11.py
+++ ModelScripts/EfficientNetFamily/SurfaceTension/11v/ModelSurfaceTensionENFV11.py
@@ -88,7 +88,7 @@ def main():
     
     output_csv = "ST_Model_Predictions.csv"
     output_training = "Surface Tension (mN/m)"
-    batch_size = 64
+    batch_size = 32
     model_name="SurfaceTensionENF4"
     image_size = (800, 800)
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
