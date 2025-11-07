92548e0a65b8a613c442dc89e362249c4a27b617
jordan wang <jordanw7@hawaii.edu>
2025-11-07 10:38:46 -1000

    push for lower data size

--- ModelScripts/EfficientNetFamily/SurfaceTension/10v/ModelSurfaceTensionENFV10.py
+++ ModelScripts/EfficientNetFamily/SurfaceTension/10v/ModelSurfaceTensionENFV10.py
@@ -88,7 +88,7 @@ def main():
     
     output_csv = "ST_Model_Predictions.csv"
     output_training = "Surface Tension (mN/m)"
-    batch_size = 64
+    batch_size = 32
     model_name="SurfaceTensionENF4"
     image_size = (510, 384)
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
