f526ef72ded30068dd2ca3ff3e5297c0f55c9353
jordan wang <jordanw7@hawaii.edu>
2025-11-12 20:31:52 -1000

    even smaller v10

--- ModelScripts/EfficientNetFamily/SurfaceTension/10v/ModelSurfaceTensionENFV10.py
+++ ModelScripts/EfficientNetFamily/SurfaceTension/10v/ModelSurfaceTensionENFV10.py
@@ -90,7 +90,7 @@ def main():
     output_training = "Surface Tension (mN/m)"
     batch_size = 32
     model_name="SurfaceTensionENF4"
-    image_size = (800, 800)
+    image_size = (600, 600)
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
     "best_SurfaceTensinoENFv10.keras",
     monitor="val_loss",
