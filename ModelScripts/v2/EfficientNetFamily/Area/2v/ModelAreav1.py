5ceafe29c6dbe23435dc5f4bd9d1e19dcf6157d1
jordan wang <jordanw7@hawaii.edu>
2025-12-09 09:56:52 -1000

    area v2 smaller

--- ModelScripts/v2/EfficientNetFamily/Area/2v/ModelAreav1.py
+++ ModelScripts/v2/EfficientNetFamily/Area/2v/ModelAreav1.py
@@ -88,7 +88,7 @@ def main():
     
     output_csv = "ST_Model_Predictions.csv"
     output_training = "Area (cm^2)"
-    batch_size = 128
+    batch_size = 64
     model_name="SurfaceTensionENF4"
     image_size = (640, 640)
     checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
